4c1c67fe57237ec32800a2b850952f74c95b5591
adriantene <46764736+AdrianTene38@users.noreply.github.com>
2019-11-25 02:57:39 -0500

    cambio

--- public/controladores/crear_usuario.php
+++ public/controladores/crear_usuario.php
@@ -1,40 +1,42 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Crear Nuevo Usuario</title>
-        <link type="text/css" rel="stylesheet" href="../css/crear_usuario.css">
-    </head>
-    <body>
-        <form class="box">
-        <?php
-            //Incluir conexion a la base de datos
-            include '../../config/conexionBD.php';
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Crear nuevo documento</title>
+     <style type = "text/css" ref= "stylesheet">
+     error{
+         color: red;
+     }
+     </style>
+ </head>
+ <body>
+    <?php
+    include '../../config/conexionBD.php';
 
-            $cedula = isset($_POST["cedula"]) ? trim($_POST["cedula"]) : null;
-            $nombres = isset($_POST["nombres"]) ? mb_strtoupper(trim($_POST["nombres"]), 'UTF-8') : null;
-            $apellidos = isset($_POST["apellidos"]) ? mb_strtoupper(trim($_POST["apellidos"]), 'UTF-8') : null;
-            $direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]), 'UTF-8') : null;
-            $telefono = isset($_POST["telefono"]) ? trim($_POST["telefono"]) : null;
-            $correo = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
-            $fechaNacimiento = isset($_POST["fechaNacimiento"]) ? trim($_POST["fechaNacimiento"]) : null;
-            $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
+    $cedula = isset($_POST["cedula"]) ?trim($_POST["cedula"]): null;
+    $nombres = isset($_POST["nombre"]) ? mb_strtoupper(trim($_POST["nombre"]), 'UTF-8'): null;
+    $apellidos = isset($_POST["apellido"]) ? mb_strtoupper(trim($_POST["apellido"]), 'UTF-8'): null;
+    $direccion = isset($_POST["direccion"]) ? mb_strtoupper(trim($_POST["direccion"]), 'UTF-8'): null;
+    $telefono = isset($_POST["telefono"]) ? mb_strtoupper(trim($_POST["telefono"]), 'UTF-8'): null;
+    $correo= isset($_POST["email"]) ? mb_strtoupper(trim($_POST["email"]), 'UTF-8'): null;
+    $fD= isset($_POST["fD"]) ? mb_strtoupper(trim($_POST["fD"]), 'UTF-8'): null;
+    $password= isset($_POST["password"]) ? mb_strtoupper(trim($_POST["password"]), 'UTF-8'): null;
+    $sql = "INSERT INTO usuario VALUES (0, '$cedula', '$nombres', '$apellidos', '$direccion', '$telefono', '$correo', MD5('$password'), '$fD', 'N', null, null, 2, null)";
 
-            $sql = "INSERT INTO usuario VALUES (0, '$cedula', '$nombres', '$apellidos', '$direccion', '$telefono', '$correo', MD5('$contrasena'), '$fechaNacimiento', 'N', null, null, 2, null)";
+    if ($conn->query($sql)==TRUE){
+        echo"<p>Se han creado los datos personales correctamente!!!</p>";
+    }else{
+        if ($conn->errno == 1062){
+            echo"<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema</p>";
+        }else{
+            echo"<p class='error'>Error: " .mysqli_error($conn)."</p>";
+        }
+    }
 
-            if ($conn->query($sql)==TRUE){
-                echo"<p>Se han creado los datos personales correctamente!!!</p>";
-            }else{
-                if ($conn->errno == 1062){
-                    echo"<p class='error'>La persona con la cedula $cedula ya esta registrada en el sistema</p>";
-                }else{
-                    echo"<p class='error'>Error: " .mysqli_error($conn)."</p>";
-                }
-            }
-
-            $conn->close();
-            echo"<a href='../vista/crear_usuario.html'>Regresar</a>";
-        ?>
-    </form>
-    </body>
-</html>
+    $conn->close();
+    echo"<a href='../vista/crear_usuario.html'>Regresar</a>";
+?>
+ </body>
+ </html>
